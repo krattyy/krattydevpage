07fce4b2d4cc80742b5fb03368a0f3231dec4642
Kratty <kizilirmak525@gmail.com>
2025-11-30 22:23:11 +0300

    style: Clean up Footer component by removing unnecessary text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>
-        Copyright © Tuna Enes Kızılırmak 2025. Tüm hakları saklıdır.
-        asdasdasdasd
-      </p>
+      <p>Copyright © Tuna Enes Kızılırmak 2025. Tüm hakları saklıdır.</p>
     </footer>
   );
 }
